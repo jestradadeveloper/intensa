3b3ab0510da011ca4d26ddb86a18106f0b79fa50
Jose Estrada <webdeveloper@vgloreto.com>
2020-03-20 13:21:46 -0600

    category style

--- category.php
+++ category.php
@@ -1,10 +1,19 @@
 <?php get_header(); ?>
+<section class="container my-2 ">
+  <div class="row">
+    <div class="col-12">
+	<h1 class="title-internal text-center py-4"><?php single_cat_title(); ?></h1>
+    </div>
+  </div>
 
+
+
+</section>
+<div class="section container mb-5">
 	<main role="main">
 		<!-- section -->
 		<section>
-
-			<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
+		
 
 			<?php get_template_part('loop'); ?>
 
@@ -13,7 +22,6 @@
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
+	</div>
 
 <?php get_footer(); ?>
